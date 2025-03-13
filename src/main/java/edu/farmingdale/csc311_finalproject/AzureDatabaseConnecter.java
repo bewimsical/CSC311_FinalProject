9f733b9654fcf63e00dfb906d93be3297862d9f1
Qauntum12 <kennethlegare5@gmail.com>
2025-03-13 11:08:27 -0400

    Initial Azure Connection

---
+++ src/main/java/edu/farmingdale/csc311_finalproject/AzureDatabaseConnecter.java
@@ -0,0 +1,30 @@
+package edu.farmingdale.csc311_finalproject;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class AzureDatabaseConnecter {
+
+    public static void main(String[] args) {
+        String host = "your_server.database.windows.net"; // Replace with your server name
+        String database = "your_database"; // Replace with your database name
+        String user = "your_username"; // Replace with your username
+        String password = "your_password"; // Replace with your password
+        String url = String.format("jdbc:sqlserver://%s:1433;database=%s;user=%s;password=%s;encrypt=true;trustServerCertificate=false;hostNameInCertificate=*.database.windows.net;", host, database, user, password);
+
+        try (Connection connection = DriverManager.getConnection(url);
+             Statement statement = connection.createStatement();
+             ResultSet resultSet = statement.executeQuery("SELECT * FROM your_table")) { //replace with your table name
+
+            while (resultSet.next()) {
+                System.out.println(resultSet.getString("your_column"));//replace with your column name
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module edu.farmingdale.csc311_finalproject {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.sql;
 
 
     opens edu.farmingdale.csc311_finalproject to javafx.fxml;

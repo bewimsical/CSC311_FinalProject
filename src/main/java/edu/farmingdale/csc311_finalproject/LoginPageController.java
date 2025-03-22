9798649026c062b001051955684b5eb94976edef
joshs <samsonknight909@gmail.com>
2025-03-22 14:48:24 -0400

    Changed the whole thing from split to regular pane

--- src/main/java/edu/farmingdale/csc311_finalproject/LoginPageController.java
+++ src/main/java/edu/farmingdale/csc311_finalproject/LoginPageController.java
@@ -1,38 +1,11 @@
 package edu.farmingdale.csc311_finalproject;
 
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.control.SplitPane;
-import javafx.scene.control.TextField;
-import javafx.scene.text.Text;
 
 public class LoginPageController {
 
     @FXML
-    private TextField emailAddressTextField;
-
-    @FXML
-    private TextField passwordTextField;
-
-    @FXML
-    private Button signInButton;
-
-    @FXML
-    private Text signInFont;
-
-    @FXML
-    private SplitPane splitPaneGameNight;
-
-    @FXML
     private Label welcomeText;
-
-    @FXML
-    void signInOnButton(ActionEvent event) {
-
-
-
-    }
 
 }

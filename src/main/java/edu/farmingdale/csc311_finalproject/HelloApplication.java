ccc60dc5e4704bc1909150a0856b417c929cc547
joshs <samsonknight909@gmail.com>
2025-03-23 11:12:50 -0400

    Removed redundant code

--- src/main/java/edu/farmingdale/csc311_finalproject/HelloApplication.java
+++ src/main/java/edu/farmingdale/csc311_finalproject/HelloApplication.java
@@ -11,11 +11,7 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("LoginPage.fxml"));
-        //Profile page FXML
-        //FXMLLoader fxmlProfileLoader = new FXMLLoader(HelloApplication.class.getResource("ProfilePage.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 530, 500);
-       //Profile page layout
-        //Scene scene = new Scene(fxmlLoader.load(), 650, 600);
         stage.setTitle("");
         stage.setScene(scene);
         stage.show();
--- src/main/java/edu/farmingdale/csc311_finalproject/LoginPageController.java
+++ src/main/java/edu/farmingdale/csc311_finalproject/LoginPageController.java
@@ -43,30 +43,26 @@ public class LoginPageController {
 
     @FXML
     private void handleSignIn() {
-        // Check if both fields are filled (non-empty)
+
         if (!emailTextField.getText().isEmpty() && !passwordTextField.getText().isEmpty()) {
-            // Both fields are not empty, proceed with the action (e.g., sign in logic)
+
             System.out.println("Sign In successful");
 
-            // Switch to the Profile Page (New Scene)
+
             try {
-                // Load the Profile Page FXML
+
                 FXMLLoader fxmlProfileLoader = new FXMLLoader(HelloApplication.class.getResource("ProfilePage.fxml"));
 
-
-                // Create a new Scene for the profile page
                 Scene profileScene = new Scene(fxmlProfileLoader.load(), 650, 600);
 
-                // Create a new Stage (window) for the profile page
                 Stage profileStage = new Stage();
                 profileStage.setTitle("Profile Page");
                 profileStage.setScene(profileScene);
                 profileStage.show();
 
-                // Close the current window (login window)
-                Stage currentStage = (Stage) signInButton.getScene().getWindow();
-                currentStage.close(); // Close the login window
 
+                Stage currentStage = (Stage) signInButton.getScene().getWindow();
+                currentStage.close();
             } catch (Exception e) {
                 e.printStackTrace();
                 Alert alert = new Alert(Alert.AlertType.ERROR);

000307ef96c83befb6155b3b99260f98ad3a3e84
joshs <samsonknight909@gmail.com>
2025-04-19 17:02:17 -0400

    Changing the login screen again so pushing to save progress

---
+++ src/main/java/edu/farmingdale/csc311_finalproject/TestSplashController.java
@@ -0,0 +1,77 @@
+package edu.farmingdale.csc311_finalproject;
+
+import javafx.animation.PauseTransition;
+import javafx.animation.ScaleTransition;
+import javafx.animation.TranslateTransition;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+import javafx.util.Duration;
+
+import java.io.IOException;
+
+public class TestSplashController {
+
+    @FXML
+    private Text logInText;
+
+    @FXML
+    private ImageView logInBoard;
+
+    @FXML
+    private ImageView signUpBoard;
+
+    @FXML
+    void logInPage(MouseEvent event) {
+        logInBoard.setDisable(true);
+
+        // Move the board
+        TranslateTransition tt = new TranslateTransition(Duration.seconds(2), logInBoard);
+        tt.setToY(-195);
+        tt.setToX(-13);
+        tt.setCycleCount(1);
+        tt.play();
+
+        // Bring to front after 1.5 seconds
+        PauseTransition pause = new PauseTransition(Duration.seconds(1.3));
+        pause.setOnFinished(e -> logInBoard.toFront());
+        pause.play();
+
+        // After translation is done, do a scale transition
+        tt.setOnFinished(e -> {
+            ScaleTransition st = new ScaleTransition(Duration.seconds(2), logInBoard);
+            st.setToX(4.1);
+            st.setToY(5);
+            st.setCycleCount(1);
+            st.setAutoReverse(false);
+            st.play();
+            st.setOnFinished(ev2 -> {
+                try {
+                    FXMLLoader loader = new FXMLLoader(getClass().getResource("spareLoginPage.fxml"));
+                    Parent root = loader.load();
+                    Stage stage = (Stage) logInBoard.getScene().getWindow();
+                    Scene scene = new Scene(root, 650, 600);
+                    stage.setScene(scene);
+                    stage.show();
+                } catch (IOException ex) {
+                    ex.printStackTrace();
+                }
+            });
+        });
+
+    }
+
+
+    @FXML
+    void signUpPage(MouseEvent event) {
+
+    }
+
+    }
+

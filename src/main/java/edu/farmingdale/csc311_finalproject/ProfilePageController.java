24ffe1e4eb029c88b08b8067feaa98b530ec2e73
darianne123 <darianneramos@icloud.com>
2025-04-28 22:39:00 -0400

    worked on adding to the ProfilePageController mostly trying to understand

--- src/main/java/edu/farmingdale/csc311_finalproject/ProfilePageController.java
+++ src/main/java/edu/farmingdale/csc311_finalproject/ProfilePageController.java
@@ -1,34 +1,28 @@
 package edu.farmingdale.csc311_finalproject;
 
+import com.fasterxml.jackson.core.type.TypeReference;
 import javafx.fxml.FXML;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.*;
+import javafx.scene.shape.Circle;
 import javafx.scene.text.Text;
+import javafx.scene.text.TextAlignment;
 
+import java.io.IOException;
 import java.net.URL;
+import java.util.List;
+import java.util.Objects;
 import java.util.ResourceBundle;
 
+import static edu.farmingdale.csc311_finalproject.ApiClient.*;
+
 public class ProfilePageController {
+@FXML
+Circle circle_view;
 
-    @FXML
-    private Label welcomeText;
-
-    //@FXML
-    //private Text usernameLabel;
-
-    @FXML
-    private Label emailLabel;
-
-
-
-    @FXML
-    private Label usernameLabel; // or Text if you used that!
-
-    public void initialize(URL url, ResourceBundle resourceBundle) {
-        User user = Session.getInstance().getUser();
-        if (user != null) {
-            usernameLabel.setText(user.getUsername());
-        }
-    }
-
-
+@FXML
 }

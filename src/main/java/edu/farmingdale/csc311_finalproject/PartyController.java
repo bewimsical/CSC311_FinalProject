d04fafe26562177ab6a7fd56ad0234aec2dbd98e
Kathleen Wims <bewimsical@gmail.com>
2025-04-30 18:36:05 -0400

    added add guest functionality to the party page. Added equals and hashcode overrides to the user class.

--- src/main/java/edu/farmingdale/csc311_finalproject/PartyController.java
+++ src/main/java/edu/farmingdale/csc311_finalproject/PartyController.java
@@ -10,24 +10,26 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
+import javafx.scene.Node;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.MenuButton;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.*;
 import javafx.scene.shape.Circle;
 import javafx.scene.text.TextAlignment;
+import javafx.stage.Popup;
+import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.lang.reflect.Type;
 import java.net.URL;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.List;
-import java.util.Map;
-import java.util.Objects;
-import java.util.ResourceBundle;
+import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -67,39 +69,57 @@ public class PartyController implements Initializable {
     private Label selectedGamesLabel;
     @FXML
     private FlowPane selectedGamesList;
-    
+    private User currentUser;
     private Party party;
     private final ObservableList<User> guests =  FXCollections.observableArrayList();
     private final ObservableSet<Game> games =  FXCollections.observableSet();
     private final ObservableList<GameWithVotes> selectedGames =  FXCollections.observableArrayList();
+    //popup variables
+    private List <User> friends = new ArrayList<>();
+    private Popup guestPopup;
+    private User selectedGuest;
+    private Node selectedCard;
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        //keep things centered when app size changes
         gamesLabel.maxWidthProperty().bind(gamesContainer.widthProperty());
         selectedGamesLabel.maxWidthProperty().bind(selectedGamesContainer.widthProperty());
+        gamesListContainer.setFitToWidth(true);
+        gamesList.prefWidthProperty().bind(gamesListContainer.widthProperty());
+
+        //create popup for adding guests
+        guestPopup = new Popup();
+
         //todo switch to session user
         try {
-            User currentUser = sendGET(getUserUrl(1), new TypeReference<User>() {});
+            currentUser = sendGET(getUserUrl(1), new TypeReference<User>() {});
             String img = currentUser.getProfilePicUrl() != null ? currentUser.getProfilePicUrl() : "images/wizard_cat.PNG";
 
-// Load and resize image
+            // Load and resize image for nav bar
             Image image = new Image(getClass().getResource(img).toExternalForm());
             ImageView profilePic = new ImageView(image);
             profilePic.setFitWidth(circle_view.getRadius() * 2);
             profilePic.setFitHeight(circle_view.getRadius() * 2);
             profilePic.setClip(new Circle(circle_view.getRadius(), circle_view.getRadius(), circle_view.getRadius()));
 
-// Add to StackPane (on top of the Circle)
+            // Add to StackPane (on top of the Circle)
             profileContainer.getChildren().add(profilePic);
 
-// Set username
+            // Set username
             usernameLabel.setText(currentUser.getUsername());
+            try {
+                friends.addAll(Objects.requireNonNull(sendGET(getUserFriends(currentUser.getUserId()), new TypeReference<ArrayList<User>>() {})));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
 
         } catch (IOException e) {
+            currentUser = null;//TODO FIX THIS!!!!!!!
             e.printStackTrace();
         }
 
-        gamesListContainer.setFitToWidth(true);
-        gamesList.prefWidthProperty().bind(gamesListContainer.widthProperty());
+
         //todo move to a different initialize method that gets called with the party from the user page or a page that displays all parties
         try {
            party = sendGET(ApiClient.getParty(1), new TypeReference<Party>() {});
@@ -120,19 +140,9 @@ public class PartyController implements Initializable {
         } catch (IOException e) {
             e.printStackTrace();
         }
-        //todo extract to a method
+
         for (User u:guests){
-            String img = u.getProfilePicUrl() != null ? u.getProfilePicUrl() : "images/wizard_cat.PNG";
-            Image image = new Image(getClass().getResource(img).toExternalForm());
-            ImageView profilePic = new ImageView(image);
-            profilePic.setFitHeight(25);
-            profilePic.setFitWidth(25);
-            profilePic.setPreserveRatio(true);
-            profilePic.setSmooth(true);
-            Label guestName = new Label(u.getUsername());
-            guestName.getStyleClass().add("info-text");
-            HBox card = new HBox(10, profilePic, guestName);
-            VBox.setMargin(card, new Insets(5, 0, 0, 0));
+            HBox card = createGuestCard(u);
             guestList.getChildren().add(card);
 
             try {
@@ -142,69 +152,9 @@ public class PartyController implements Initializable {
                 e.printStackTrace();
             }
         }
-        //todo extract to method.
+
         for (Game g : games) {
-            String gameImg = g.getImgUrl();
-            Image gameImage = new Image(gameImg, true);
-            ImageView gamePic = new ImageView(gameImage);
-            gamePic.setFitHeight(75);
-            gamePic.setFitWidth(75);
-            gamePic.setPreserveRatio(true);
-            gamePic.setSmooth(true);
-
-            StackPane imageContainer = new StackPane(gamePic);
-            imageContainer.setPrefSize(75, 75);
-            imageContainer.setMaxSize(75, 75);
-
-            Label gameName = new Label(g.getGame_name());
-            gameName.setWrapText(true);
-            gameName.setMaxWidth(140); // adjust based on design
-            gameName.setTextAlignment(TextAlignment.LEFT);
-            gameName.setAlignment(Pos.CENTER_LEFT);
-            gameName.getStyleClass().add("game-name-text");
-
-            Label gamePlayers = new Label(g.getMinPlayers() + "-" + g.getMaxPlayers() + " players");
-            Label gameTime = new Label(g.getPlayTime() + " min");
-            gamePlayers.getStyleClass().add("info-text");
-            gameTime.getStyleClass().add("info-text");
-
-            VBox textBox = new VBox(5, gameName, gamePlayers, gameTime);
-            textBox.setAlignment(Pos.CENTER_LEFT);
-
-            HBox card = new HBox(10, imageContainer, textBox);
-            card.setAlignment(Pos.CENTER_LEFT);
-            card.getStyleClass().add("card");
-            card.setMaxWidth(230);
-            card.setMinWidth(230);
-            card.setUserData(g.getGameId());
-            card.setOnMouseClicked(event -> {
-                ObservableList<String> styleClasses = card.getStyleClass();
-                int id = (Integer) card.getUserData();
-                if (styleClasses.contains("selected")) {
-                    styleClasses.remove("selected");
-                    try {
-                        sendDELETE(deselectGame(party.getPartyId(), id));
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                    selectedGamesList.getChildren().clear();
-                    updateSelectedGames();
-
-                } else {
-                    styleClasses.add("selected");
-                    try {
-                        sendPOST(selectGame(party.getPartyId(), id), null, new TypeReference<Void>() {
-                        });
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                    selectedGamesList.getChildren().clear();
-                    updateSelectedGames();
-
-                }
-            });
-
-            FlowPane.setMargin(card, new Insets(10, 5, 0, 5));
+            HBox card = createGameCard(g);
             gamesList.getChildren().add(card);
         }
 
@@ -284,6 +234,89 @@ public class PartyController implements Initializable {
         }
     }
 
+    public HBox createGuestCard(User u){
+        String img = u.getProfilePicUrl() != null ? u.getProfilePicUrl() : "images/wizard_cat.PNG";
+        Image image = new Image(getClass().getResource(img).toExternalForm());
+        ImageView profilePic = new ImageView(image);
+        profilePic.setFitHeight(25);
+        profilePic.setFitWidth(25);
+        profilePic.setPreserveRatio(true);
+        profilePic.setSmooth(true);
+        Label guestName = new Label(u.getUsername());
+        guestName.getStyleClass().add("info-text");
+        HBox card = new HBox(10, profilePic, guestName);
+        VBox.setMargin(card, new Insets(5, 0, 0, 0));
+
+        //guestList.getChildren().add(card);
+        return card;
+    }
+
+    public HBox createGameCard(Game g){
+        String gameImg = g.getImgUrl();
+        Image gameImage = new Image(gameImg, true);
+        ImageView gamePic = new ImageView(gameImage);
+        gamePic.setFitHeight(75);
+        gamePic.setFitWidth(75);
+        gamePic.setPreserveRatio(true);
+        gamePic.setSmooth(true);
+
+        StackPane imageContainer = new StackPane(gamePic);
+        imageContainer.setPrefSize(75, 75);
+        imageContainer.setMaxSize(75, 75);
+
+        Label gameName = new Label(g.getGame_name());
+        gameName.setWrapText(true);
+        gameName.setMaxWidth(140); // adjust based on design
+        gameName.setTextAlignment(TextAlignment.LEFT);
+        gameName.setAlignment(Pos.CENTER_LEFT);
+        gameName.getStyleClass().add("game-name-text");
+
+        Label gamePlayers = new Label(g.getMinPlayers() + "-" + g.getMaxPlayers() + " players");
+        Label gameTime = new Label(g.getPlayTime() + " min");
+        gamePlayers.getStyleClass().add("info-text");
+        gameTime.getStyleClass().add("info-text");
+
+        VBox textBox = new VBox(5, gameName, gamePlayers, gameTime);
+        textBox.setAlignment(Pos.CENTER_LEFT);
+
+        HBox card = new HBox(10, imageContainer, textBox);
+        card.setAlignment(Pos.CENTER_LEFT);
+        card.getStyleClass().add("card");
+        card.setMaxWidth(230);
+        card.setMinWidth(230);
+        card.setUserData(g.getGameId());
+        card.setOnMouseClicked(event -> {
+            ObservableList<String> styleClasses = card.getStyleClass();
+            int id = (Integer) card.getUserData();
+            if (styleClasses.contains("selected")) {
+                styleClasses.remove("selected");
+                try {
+                    sendDELETE(deselectGame(party.getPartyId(), id));
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                selectedGamesList.getChildren().clear();
+                updateSelectedGames();
+
+            } else {
+                styleClasses.add("selected");
+                try {
+                    sendPOST(selectGame(party.getPartyId(), id), null, new TypeReference<Void>() {
+                    });
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                selectedGamesList.getChildren().clear();
+                updateSelectedGames();
+
+            }
+        });
+
+        FlowPane.setMargin(card, new Insets(10, 5, 0, 5));
+        return card;
+
+    }
+
     private String formatDate(LocalDateTime date){
         if (date != null) {
             DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM/dd/yyyy");
@@ -297,6 +330,100 @@ public class PartyController implements Initializable {
             return date.format(formatter);
         }
         return "no time";
+    }
+
+    @FXML
+    void addGuest(MouseEvent event) {
+        if (!guestPopup.isShowing()) {
+
+            VBox friendsList = new VBox();
+            ScrollPane friendsListContainer = new ScrollPane(friendsList);
+            friendsList.getStyleClass().add("friends-popup-list");
+            friendsListContainer.setPrefSize(200, 200);
+            friendsList.setPrefWidth(194);
+            friendsList.setMaxWidth(194);
+            //guestList.prefHeightProperty().bind(guestListContainer.heightProperty());
+            Button cancel = new Button("cancel");
+            cancel.setOnAction(e->{
+                guestPopup.hide();
+            });
+            Button add = new Button("add");
+
+            add.setOnAction(e->{
+                for(User g: guests){
+                    System.out.println(g.getUsername());
+                }
+                System.out.println(selectedGuest==null);
+                if (selectedGuest != null && !guests.contains(selectedGuest)){
+                    guests.add(selectedGuest);
+                    guestList.getChildren().add(createGuestCard(selectedGuest));
+                    try {
+                        sendPOST(addUserToParty(selectedGuest.getUserId(), party.getPartyId(), false), null, new TypeReference<Void>() {
+                        });
+                    } catch (IOException ex) {
+                        throw new RuntimeException(ex);
+                    }
+
+                    try {
+                        List<Game> friendGames = sendGET(getUserGames(selectedGuest.getUserId()), new TypeReference<List<Game>>() {});
+                        for(Game g:friendGames){
+                            gamesList.getChildren().add(createGameCard(g));
+                        }
+                    } catch (IOException | NullPointerException  ex ) {
+                        ex.printStackTrace();
+                    }
+
+
+                }
+//                selectedGuest = null;
+//                if (selectedCard != null) {
+//                    selectedCard.getStyleClass().remove("selected-card");
+//                    selectedCard = null;
+//                }
+                guestPopup.hide();
+            });
+            cancel.getStyleClass().add("friends-popup-button");
+            add.getStyleClass().add("friends-popup-button");
+            HBox buttonContainer = new HBox(20, cancel, add);
+            buttonContainer.setAlignment(Pos.CENTER);
+            VBox popup = new VBox(10, friendsListContainer, buttonContainer);
+            popup.setMargin(buttonContainer, new Insets(10, 0, 20, 0));
+            popup.getStyleClass().add("friends-popup");
+            for (User g : friends) {
+                HBox card = createGuestCard(g);
+                card.getStyleClass().add("friend-card");
+                card.setUserData(g.getUserId());
+                //add event listener
+                card.setOnMouseClicked(e -> {
+                    long id = (Long) card.getUserData();
+                    selectedGuest = friends.stream()
+                            .filter(f -> f.getUserId() == id)
+                            .findFirst()
+                            .orElse(null);
+
+
+                    if (selectedCard != null){
+                        selectedCard.getStyleClass().add("selected-friend");
+                    }
+                    selectedCard = card;
+                    selectedCard.getStyleClass().add("selected-friend");
+                    System.out.println(selectedCard == null);
+                    if (selectedCard != null){
+                        System.out.println(selectedCard.getStyleClass() );
+                    }
+
+                });
+                friendsList.getChildren().add(card);
+
+
+            }
+
+            guestPopup.getContent().add(popup);
+
+            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            guestPopup.show(stage, event.getScreenX(), event.getScreenY());
+        }
+
     }
 
 
--- src/main/java/edu/farmingdale/csc311_finalproject/User.java
+++ src/main/java/edu/farmingdale/csc311_finalproject/User.java
@@ -1,5 +1,7 @@
 package edu.farmingdale.csc311_finalproject;
 
+import java.util.Objects;
+
 public class User {
     private Long userId;
     private String username; //should we make this unique?
@@ -98,5 +100,17 @@ public class User {
 
     public void setUsername(String username) {
         this.username = username;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(userId, user.userId);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(userId);
     }
 }

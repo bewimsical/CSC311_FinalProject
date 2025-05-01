1d5c77cc970d5c309149368f8c5989d0261851a4
darianne123 <darianneramos@icloud.com>
2025-05-01 10:31:50 -0400

    changed the profile page fxml completely working on controller now

--- src/main/java/edu/farmingdale/csc311_finalproject/HelloApplication.java
+++ src/main/java/edu/farmingdale/csc311_finalproject/HelloApplication.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("LoginPage.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("ProfilePage.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 650, 600);
         stage.setScene(scene);
         stage.show();

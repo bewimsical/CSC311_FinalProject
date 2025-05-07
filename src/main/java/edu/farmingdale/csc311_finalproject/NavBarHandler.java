2423d7505e5b7a2b083665da465aaf65b75b0bf8
Katie <bewimsical@gmail.com>
2025-05-06 22:53:21 -0400

    Fully updated profile page

--- src/main/java/edu/farmingdale/csc311_finalproject/NavBarHandler.java
+++ src/main/java/edu/farmingdale/csc311_finalproject/NavBarHandler.java
@@ -89,7 +89,9 @@ public class NavBarHandler {
                     .orElseThrow(() -> new IllegalStateException("No focused window"));
 
             Parent root = FXMLLoader.load(NavBarHandler.class.getResource(fxmlFile));
-            stage.setScene(new Scene(root));
+            Scene scene = new Scene(root);
+            scene.getStylesheets().add(NavBarHandler.class.getResource("styles/party-style.css").toExternalForm());
+            stage.setScene(scene);
             stage.show();
 
         } catch (IOException exception) {
